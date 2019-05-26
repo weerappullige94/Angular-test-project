71c6e2bb3c5c28ef9ba3fea8ee178d72ae7d3141
Tharanga Weerappullige <tharangasadamali94@gmail.com>
2019-05-26 19:59:47 +0530

    correctness

--- my-app/src/app/app.module.ts
+++ my-app/src/app/app.module.ts
@@ -5,14 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
-import { SetmycolorDirective } from './derectives/setmycolor.directive';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
     FooterComponent,
-    SetmycolorDirective
+    
   ],
   imports: [
     BrowserModule,
